2d4a3514bdd9174afcd666e88924cbc3f1b898f5
Syaula <syaulabalqiea@gmail.com>
2026-06-08 12:59:30 +0700

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,1 +1,15 @@
+#ifndef SORTING_H
+#define SORTING_H
 
+#include "data.h"
+
+/* Insertion Sort */
+void insertionSort(WordFreq arr[], int n);
+
+/* Quick Sort */
+void quickSort(WordFreq arr[], int low, int high);
+
+/* Heap Sort */
+void heapSort(WordFreq arr[], int n);
+
+#endif

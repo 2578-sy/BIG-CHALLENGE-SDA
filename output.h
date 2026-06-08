1ea350ffb9fd92fcf72c24d2bae6b271f437f232
Syaula <syaulabalqiea@gmail.com>
2026-06-08 13:11:49 +0700

    Update output.h

--- output.h
+++ output.h
@@ -1,1 +1,20 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
 
+#include "data.h"
+
+/* Simpan hasil sorting ke file */
+void saveToFile(
+    char filename[],
+    WordData data[],
+    int totalWords,
+    double timeTaken
+);
+
+/* Tampilkan Top-K ke layar */
+void printTopK(
+    WordData data[],
+    int k
+);
+
+#endif

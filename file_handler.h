7b3db85451611cb6b36480c75426ab7ea0b3de86
Althaffia <althafpuput@gmail.com>
2026-06-08 12:56:46 +0700

    file_handler.h

--- file_handler.h
+++ file_handler.h
@@ -1,1 +1,28 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
 
+// batas maksimum kata
+#define MAX_WORDS 150000
+
+// panjang maksimum kata
+#define MAX_LENGTH 100
+
+// struct data kata dan frekuensi
+typedef struct {
+    char word[MAX_LENGTH];
+    long long freq;
+} WordData;
+
+// array global data kata
+extern WordData data[MAX_WORDS];
+
+// membaca file vocabulary
+void loadVocabulary(char vocabFile[]);
+
+// membaca file docword
+void loadDocword(char docwordFile[]);
+
+// mengambil total kata
+int getTotalWords();
+
+#endif

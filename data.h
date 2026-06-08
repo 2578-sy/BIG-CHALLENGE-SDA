6fb5d8dd575b451c7192b5eee06e78c6db79a6a7
Althaffia <althafpuput@gmail.com>
2026-06-08 12:59:13 +0700

    data.h

--- data.h
+++ data.h
@@ -1,1 +1,16 @@
+#ifndef DATA_H
+#define DATA_H
 
+// batas maksimum kata
+#define MAX_WORDS 150000
+
+// panjang maksimum kata
+#define MAX_LENGTH 100
+
+// struct penyimpanan kata dan frekuensi
+typedef struct {
+    char word[MAX_LENGTH];
+    long long freq;
+} WordData;
+
+#endif
